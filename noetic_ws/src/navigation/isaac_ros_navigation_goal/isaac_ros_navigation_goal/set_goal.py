b58b92886c1a57f52e4ed29f76afdc02a46b3c04
MrKeith99 <kvalentincardenas@hotmail.com>
2025-01-02 20:57:53 +0900

    [UPD] ns based

--- noetic_ws/src/navigation/isaac_ros_navigation_goal/isaac_ros_navigation_goal/set_goal.py
+++ noetic_ws/src/navigation/isaac_ros_navigation_goal/isaac_ros_navigation_goal/set_goal.py
@@ -15,11 +15,11 @@ class SetNavigationGoal:
         self.__goal_generator = self.__create_goal_generator()
         action_server_name = rospy.get_param("action_server_name", "move_base")
         self._action_client = actionlib.SimpleActionClient(action_server_name, MoveBaseAction)
-        self.MAX_ITERATION_COUNT = rospy.get_param("iteration_count", 1)
+        self.MAX_ITERATION_COUNT = rospy.get_param("/iteration_count", 1)
         assert self.MAX_ITERATION_COUNT > 0
         self.curr_iteration_count = 1
         self.__initial_goal_publisher = rospy.Publisher("initialpose", PoseWithCovarianceStamped, queue_size=1)
-        self.__initial_pose = rospy.get_param("initial_pose", None)
+        self.__initial_pose = rospy.get_param("/initial_pose", None)
         self.__is_initial_pose_sent = True if self.__initial_pose is None else False
 
     def __send_initial_pose(self):
@@ -45,18 +45,19 @@ class SetNavigationGoal:
         """
         Sends the goal to the action server.
         """
-        if not self.__is_initial_pose_sent:
-            rospy.loginfo("Sending initial pose")
-            self.__send_initial_pose()
-            self.__is_initial_pose_sent = True
+        # if not self.__is_initial_pose_sent:
+        #     rospy.loginfo("Sending initial pose")
+        #     self.__send_initial_pose()
+        #     self.__is_initial_pose_sent = True
 
-            # Assumption is that initial pose is set after publishing first time in this duration.
-            # Can be changed to more sophisticated way. e.g. /particlecloud topic has no msg until
-            # the initial pose is set.
-            rospy.sleep(10)
-            rospy.loginfo("Sending first goal")
+        #     # Assumption is that initial pose is set after publishing first time in this duration.
+        #     # Can be changed to more sophisticated way. e.g. /particlecloud topic has no msg until
+        #     # the initial pose is set.
+        #     rospy.sleep(10)
+        #     rospy.loginfo("Sending first goal")
 
         self._action_client.wait_for_server()
+        rospy.loginfo("Sever is up and running")
         goal_msg = self.__get_goal()
 
         if goal_msg is None:
@@ -125,25 +126,25 @@ class SetNavigationGoal:
         pass
 
     def __create_goal_generator(self):
-        goal_generator_type = rospy.get_param("goal_generator_type", "RandomGoalGenerator")
+        goal_generator_type = rospy.get_param("/goal_generator_type", "RandomGoalGenerator")
         goal_generator = None
         if goal_generator_type == "RandomGoalGenerator":
-            if rospy.get_param("map_yaml_path", None) is None:
+            if rospy.get_param("/map_yaml_path", None) is None:
                 rospy.loginfo("Yaml file path is not given. Returning..")
                 sys.exit(1)
 
-            yaml_file_path = rospy.get_param("map_yaml_path", None)
+            yaml_file_path = rospy.get_param("/map_yaml_path", None)
             grid_map = GridMap(yaml_file_path)
-            obstacle_search_distance_in_meters = rospy.get_param("obstacle_search_distance_in_meters", 0.2)
+            obstacle_search_distance_in_meters = rospy.get_param("/obstacle_search_distance_in_meters", 0.2)
             assert obstacle_search_distance_in_meters > 0
             goal_generator = RandomGoalGenerator(grid_map, obstacle_search_distance_in_meters)
 
         elif goal_generator_type == "GoalReader":
-            if rospy.get_param("goal_text_file_path", None) is None:
+            if rospy.get_param("/goal_text_file_path", None) is None:
                 rospy.loginfo("Goal text file path is not given. Returning..")
                 sys.exit(1)
 
-            file_path = rospy.get_param("goal_text_file_path", None)
+            file_path = rospy.get_param("/goal_text_file_path", None)
             goal_generator = GoalReader(file_path)
         else:
             rospy.loginfo("Invalid goal generator specified. Returning...")
